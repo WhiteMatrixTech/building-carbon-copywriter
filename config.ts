ada1a356be388e95a3588af45936cc3c296b190a
xia <xiadd0102@gmail.com>
2022-03-24 15:55:37 +0800

    添加基本文案

---
+++ config.ts
@@ -0,0 +1,25 @@
+/**
+ * 首页相关文案
+ */
+export const home = {
+    /**
+     * 首页轮播图
+     */
+    carousels: [
+        {
+            title: 'xxx',
+            subtitle: 'xxxxxx',
+            bgImage: 'xxx'
+        }
+    ],
+    /**
+     * 项目快速入口
+     */
+    projects: [
+        {
+            cover: 'xxx',
+            title: 'xxx',
+            link: 'xxxx'
+        }
+    ]
+}
