fd047761a3430de1b2196c5160e4d8a2ddfb8d43
xia <xiadd0102@gmail.com>
2022-03-25 11:23:43 +0800

    update copywriter

--- config.ts
+++ config.ts
@@ -2,24 +2,59 @@
  * 首页相关文案
  */
 export const home = {
-    /**
-     * 首页轮播图
-     */
-    carousels: [
-        {
-            title: 'xxx',
-            subtitle: 'xxxxxx',
-            bgImage: 'xxx'
-        }
-    ],
-    /**
-     * 项目快速入口
-     */
-    projects: [
-        {
-            cover: 'xxx',
-            title: 'xxx',
-            link: 'xxxx'
-        }
-    ]
+  /**
+   * 首页轮播图
+   */
+  carousels: [
+    {
+      title: 'xxx',
+      subtitle: 'xxxxxx',
+      bgImage: 'xxx'
+    }
+  ],
+  /**
+   * 项目快速入口
+   */
+  projects: [
+    {
+      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      title: '南京金融城超高层监测项目',
+      link: '/'
+    },
+    {
+      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      title: '南京金融城超高层监测项目',
+      link: '/'
+    },
+    {
+      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      title: '南京金融城超高层监测项目',
+      link: '/'
+    },
+    {
+      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      title: '南京金融城超高层监测项目',
+      link: '/'
+    },
+    {
+      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      title: '南京金融城超高层监测项目',
+      link: '/'
+    },
+    {
+      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      title: '南京金融城超高层监测项目',
+      link: '/'
+    },
+    {
+      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      title: '南京金融城超高层监测项目',
+      link: '/'
+    },
+    {
+      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      title: '南京金融城超高层监测项目',
+      link: '/'
+    }
+  ]
 }
