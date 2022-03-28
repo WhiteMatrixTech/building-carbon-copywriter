7c77bbeb938d5600be69972da2b5081bfb9a9eff
Ly <34117007+LyingZz@users.noreply.github.com>
2022-03-28 11:12:39 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -16,44 +16,84 @@ export const home = {
    * 项目快速入口
    */
   projects: [
-    {
-      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+   {
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
       title: '南京金融城超高层监测项目',
       link: '/'
     },
     {
-      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
-      title: '南京金融城超高层监测项目',
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: '南京长江大桥项目',
       link: '/'
     },
     {
-      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
-      title: '南京金融城超高层监测项目',
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: '南京跨省高架项目',
       link: '/'
     },
     {
-      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
-      title: '南京金融城超高层监测项目',
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: '南京中央商场项目',
       link: '/'
     },
     {
-      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
-      title: '南京金融城超高层监测项目',
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: '南部新城白******末站建设工程',
       link: '/'
     },
     {
-      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
-      title: '南京金融城超高层监测项目',
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: 'NO.202******地产开发项目',
       link: '/'
     },
     {
-      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
-      title: '南京金融城超高层监测项目',
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: '栖霞区******信工程',
       link: '/'
     },
     {
-      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
-      title: '南京金融城超高层监测项目',
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: '栖霞区妇******接入工程',
+      link: '/'
+    },
+        {
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: 'NO.2021G******10KV杆线迁移',
+      link: '/'
+    },
+    {
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: '羊山公园景观******目-西侧地块一',
+      link: '/'
+    },
+    {
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: 'NO.******19地块项目主体工程',
+      link: '/'
+    },
+    {
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: '南京中央商场项目',
+      link: '/'
+    },
+    {
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: '南部新城白******末站建设工程',
+      link: '/'
+    },
+    {
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: 'NO.202******地产开发项目',
+      link: '/'
+    },
+    {
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: '南京邮电大******力外线接入',
+      link: '/'
+    },
+    {
+      cover: 'https://matrixlabs-resource.s3.ap-northeast-1.amazonaws.com/Test+img/Carbon+emissions/HomePage/Rectangle+5.png',
+      title: '浦口区北******学建设工程',
       link: '/'
     }
   ]
