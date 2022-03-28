0792318ad028d82b1afcdf10c595edfd7731363f
临安 <2591088659@qq.com>
2022-03-28 09:46:07 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -55,6 +55,26 @@ export const home = {
       cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
       title: '南京金融城超高层监测项目',
       link: '/'
+    },
+        {
+      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      title: '南京金融城超高层监测项目',
+      link: '/'
+    },
+    {
+      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      title: '南京金融城超高层监测项目',
+      link: '/'
+    },
+    {
+      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      title: '南京金融城超高层监测项目',
+      link: '/'
+    },
+    {
+      cover: 'https://iph.href.lu/200x200?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&fg=666666&bg=cccccc',
+      title: '南京金融城超高层监测项目',
+      link: '/'
     }
   ]
 }
